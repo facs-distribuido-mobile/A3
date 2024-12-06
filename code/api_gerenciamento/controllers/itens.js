face891745958d2136d004c19ae882b5512e6eda
Marcelo <19808040+mmcav@users.noreply.github.com>
2024-12-05 23:56:54 -0300

    fix: correções relativas aos itens e estoque

--- code/api_gerenciamento/controllers/itens.js
+++ code/api_gerenciamento/controllers/itens.js
@@ -5,22 +5,24 @@ const Item = require('../models/Item');
 module.exports = app => {
     // Retornar todas as entradas da tabela itens
     app.get('/itens', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         try {
             ItemDao.all((err, itens) => {
-                res.header("Access-Control-Allow-Origin", "*");
                 if(err === null) {
                     return res.status(200).send(itens);
                 } else {
-                    return res.status(404).send(`Erro: Not Found`);
+                    return res.status(404).send({ erro: 'Nenhum item encontrado.' });
                 }
-            })
+            });
         } catch (err) {
-            return res.status(500).send(`Erro: ${err.message}`);
+            console.log(err);
+            return res.status(500).send({ erro: 'Erro no servidor.' });
         }
-    })
+    });
 
     // Retornar uma entrada da tabela item, por id
     app.get('/itens/:id', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         const idNum = req.params.id;
 
         try {
@@ -28,83 +30,86 @@ module.exports = app => {
                 if(err === null) {
                     return res.status(200).send(item);
                 } else {
-                    return res.status(404).send(`Erro: Not Found`);
+                    return res.status(404).send({ erro: `Item de id '${idNum}' não encontrado.` });
                 }
-            })
+            });
         } catch (err) {
-            return res.status(500).send(`Erro: ${err.message}`);
+            console.log(err);
+            return res.status(500).send({ erro: 'Erro no servidor.' });
         }
-    })
+    });
 
     // Adicionar uma entrada da tabela item
     app.post('/itens', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         const newItem = req.body;
 
         if(newItem.nome === undefined || newItem.preco === undefined) {
-            return res.status(400).send(`Os campos nome e preço são obrigatórios!`)
+            return res.status(400).send({ erro: `Os campos nome e preço são obrigatórios!` });
         } else {
             try {
                 const item = new Item(newItem.nome, newItem.preco);
 
                 if(item.nome === undefined || item.preco === undefined) {
-                    return res.status(400).send(`Erro: dados de nome e/ou preço incorretos!`)
+                    return res.status(400).send({ erro: `Dados de nome e/ou preço incorretos!` });
                 }
 
                 ItemDao.adicionar(item);
-                return res.status(200).send(`Item cadastrado com sucesso:
-                                              nome: ${item.nome},
-                                                preço: ${middleware.centsToReal(item.preco)}`);
+                return res.status(201).send({ mensagem: 'Item cadastrado com sucesso!', item });
             } catch (err) {
-                return res.status(500).send(`Erro: ${err.message}`)
+                console.log(err);
+                return res.status(500).send({ erro: 'Erro no servidor.' });
             }
         }
-    })
+    });
 
     // Modificar o nome ou preço de uma entrada da tabela item, por id
     app.put('/itens/:id', (req,res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         const idItem = req.params.id;
         const newItem = req.body;
 
         if(newItem.nome === undefined || newItem.preco === undefined) {
-            return res.status(400).send(`Os campos de nome e preço são obrigatórios!`);
+            return res.status(400).send({ erro: `Os campos nome e preço são obrigatórios!` });
         } else {
             try {
                 ItemDao.get(idItem, (err, item_cadastrado) => {
                     if(item_cadastrado === null || err !== null) {
-                        return res.status(400).send(`Erro: item não encontrado no cadastro!`);
+                        return res.status(404).send({ erro: `Item não encontrado no cadastro!` });
                     }
                     //
                     const item = new Item(newItem.nome, newItem.preco);
                     if(item.nome === undefined || item.preco === undefined) {
-                        return res.status(400).send(`Erro: dado de nome e/ou preço incorretos!`);
+                        return res.status(400).send({ erro: `Dados de nome e/ou preço incorretos!` });
                     }
 
                     ItemDao.modificar(idItem, item);
-                    return res.status(200).send(`Item modificado com sucesso:
-                                                 nome do item: ${item.nome},
-                                                 preço do item: ${middleware.centsToReal(item.preco)}`);
-                })
+                    return res.status(200).send({ mensagem: 'Item modificado com sucesso!', item });
+                });
             } catch (err) {
-                return res.status(500).send(`Erro: ${err.message}`);
+                console.log(err);
+                return res.status(500).send({ erro: 'Erro no servidor.' });
             }
         }
 
-    })
+    });
 
     // Excluir uma entrada de item do cadastro, por id
     app.delete('/itens/:id', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         const idNum = req.params.id;
 
         try {
             ItemDao.delete(idNum, (err, item) => {
                 if(err === null) {
-                    return res.status(200).send(`Item de id ${idNum} excluído com sucesso!`);
+                    return res.status(200).send({ mensagem: `Item de id '${idNum}' excluído com sucesso!`});
                 } else {
-                    return res.status(404).send(`Erro: Not Found`);
+                    return res.status(404).send({ erro: `Item de id '${idNum}' não encontrado.` });
                 }
-            })
+            });
         } catch (err) {
-            return res.status(500).send(`Erro: ${err.message}`);
+            console.log(err);
+            return res.status(500).send({ erro: 'Erro no servidor.' });
         }
-    })
+    });
 }
--- code/api_gerenciamento/dao/ItemEstoqueDao.js
+++ code/api_gerenciamento/dao/ItemEstoqueDao.js
@@ -4,7 +4,7 @@ const middleware = require('../middlewares/general');
 class ItemEstoqueDao {
 
     total(callback) {
-        let sql = `SELECT count(*) as count FROM estoque`;
+        const sql = `SELECT count(*) as count FROM estoque`;
 
         dbConnection.createConnection().query(sql, [], (err, total) => {
             if (err || total === undefined) {
@@ -12,11 +12,11 @@ class ItemEstoqueDao {
             } else {
                 callback(null, total.count);
             }
-        })
+        });
     }
 
     all(callback) {
-        let sql = `SELECT estoque.id, itens.nome, itens.preco, estoque.quantidade_atual FROM estoque LEFT JOIN itens on estoque.id_item = itens.id`;
+        const sql = `SELECT estoque.id, itens.nome, itens.preco, estoque.quantidade_atual FROM estoque LEFT JOIN itens on estoque.id_item = itens.id`;
 
         dbConnection.createConnection().query(sql, [], (err, itens_estoque) => {
             if(err || itens_estoque === undefined) {
@@ -25,9 +25,9 @@ class ItemEstoqueDao {
                 itens_estoque.forEach(item_estoque => {
                     item_estoque.preco = middleware.centsToReal(item_estoque.preco)
                 });
-                callback(null, itens_estoque)
+                callback(null, itens_estoque);
             }
-        })
+        });
     }
 
     get(id, callback) {
@@ -41,7 +41,7 @@ class ItemEstoqueDao {
                 itens_estoque[0].preco = middleware.centsToReal(itens_estoque[0].preco);
                 callback(null, itens_estoque[0]);
             }
-        })
+        });
     }
 
     adicionar(item_estoque) {
@@ -59,7 +59,7 @@ class ItemEstoqueDao {
     }
 
     delete(id, callback) {
-        let sql = `DELETE FROM estoque WHERE id = ?`;
+        const sql = `DELETE FROM estoque WHERE id = ?`;
 
         dbConnection.createConnection().query(sql, [id], (err, item) => {
             if(err || item.affectedRows === 0) {
@@ -67,7 +67,7 @@ class ItemEstoqueDao {
             } else {
                 callback(null, item);
             }
-        })
+        });
     }
 
 }
