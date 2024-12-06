7280721c7e130b7b0927c8607bfffbf44d808254
Marcelo <19808040+mmcav@users.noreply.github.com>
2024-12-06 13:11:48 -0300

    fix: revisão da api do relatório

--- code/api_relatorio/controllers/rEstoque.js
+++ code/api_relatorio/controllers/rEstoque.js
@@ -3,37 +3,41 @@ const REstoqueDao = require('../dao/REstoqueDao');
 module.exports = app => {
 
     app.get('/relatorios/estoque/baixo', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         const lowNum = 50;
 
         try {
             REstoqueDao.getLowEstoque(lowNum,(err, result) => {
                 if(err !== null || result === undefined) {
-                    return res.status(500).send(`Erro ao consultar banco de dados`);
+                    return res.status(500).send({ erro: `Erro ao consultar banco de dados.` });
                 } else if (result.length === 0) {
-                    return res.status(404).send(`Não há itens com estoque baixo!`);
+                    return res.status(404).send({ erro: `Não há itens com estoque baixo!` });
                 } else {
                     return res.status(200).send(result);
                 }
-            })
+            });
         } catch (err) {
-            return res.status(500).send(`Erro: ${err.message}`);
+            console.log(`Erro: ${err}`);
+            return res.status(500).send({ erro: 'Erro no servidor.' });
         }
     });
 
     app.get('/relatorios/estoque/zero', (req,res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         try {
             REstoqueDao.getZeroEstoque((err, result) => {
                 if(err !== null || result === undefined) {
-                    return res.status(500).send(`Erro ao consultar banco de dados`);
+                    return res.status(500).send({ erro: `Erro ao consultar banco de dados.` });
                 } else if (result.length === 0) {
-                    return res.status(404).send(`Não há itens com estoque ZERO!`);
+                    return res.status(404).send({ erro: `Não há itens com estoque ZERO!` });
                 } else {
                     return res.status(200).send(result);
                 }
-            })
+            });
         } catch (err) {
-            return res.status(500).send(`Erro: ${err.message}`);
+            console.log(`Erro: ${err}`);
+            return res.status(500).send({ erro: 'Erro no servidor.' });
         }
-    })
+    });
 
 }
